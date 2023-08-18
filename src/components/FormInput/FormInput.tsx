78acd45eb211e8e366a371177412cf3e58f8b0d2
benedict.unachukwu <nb1507@jdwilliamsgroup.com>
2023-08-18 17:55:24 +0100

    fix heading

--- src/components/FormInput/FormInput.tsx
+++ src/components/FormInput/FormInput.tsx
@@ -20,7 +20,7 @@ const FormInput = () => {
     <div className={style.container}>
       <div className={style.wrapper}>
         <div className="title-form">
-          <h2>Search pepoles names based on nationality</h2>
+          <h2>Search person's names based on nationality</h2>
           <div style={{ marginBottom: "20px" }}>
             <form name="nationalities-form" onSubmit={handleSubmit}>
               <input
